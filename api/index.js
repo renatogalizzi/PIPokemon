7b5795ae6586f4c43a6bf9d7f3209a154b759854
RenatoGalizzi <regalizzi@gmail.com>
2023-11-18 17:57:16 -0300

    Llenar tabla type_pokemon al iniciar el server

--- api/index.js
+++ api/index.js
@@ -1,4 +1,3 @@
-
 //                       _oo0oo_
 //                      o8888888o
 //                      88" . "88
@@ -18,12 +17,27 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+const getTypesPokemons = require("./src/controllers/getTypesPokemons.js");
 
 // Syncing all the models at once.
-conn.sync({ alter: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('Server listening at 3001'); // eslint-disable-line no-console
+conn
+  .sync({ force: true })
+  .then(() => {
+    getTypesPokemons();
+  })
+  .then(() => {
+    server.listen(3001, () => {
+      console.log("Server listening at 3001");
+    });
+  })
+  .catch((err) => {
+    console.log("fallo algo");
   });
-});
+
+// conn.sync({ alter: true }).then(() => {
+//   server.listen(3001, () => {
+//     console.log('Server listening at 3001'); // eslint-disable-line no-console
+//   });
+// });
